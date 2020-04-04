84ace01e504f1f233ab8e4d5f5b08f035941b861
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-04-04 20:40:21 +0000

    Automatically backed up by Learn

--- db/migrate/20200404203957_create_songs.rb
+++ db/migrate/20200404203957_create_songs.rb
@@ -1,6 +1,15 @@
 class CreateSongs < ActiveRecord::Migration[5.0]
   def change
     create_table :songs do |t|
+      create_table :songs do |t|
+     t.string :title
+     t.boolean :released
+     t.integer :release_year
+     t.string :artist_name
+     t.string :genre
+
+     t.timestamps
+   end
     end
   end
 end
